7f85f5b62f7875f4c3c2b1503160608b71fc4d91
neobuddy89 <neobuddy89@gmail.com>
2017-02-13 14:38:18 +0000

    Clean up and fix deviceinfo and dev settings
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -702,7 +702,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
 
         final ContentResolver cr = getActivity().getContentResolver();
         mLastEnabledState = Settings.Global.getInt(cr,
-                Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0) != 0;
+                Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 1) != 1;
         mSwitchBar.setChecked(mLastEnabledState);
         setPrefsEnabledState(mLastEnabledState);
 
@@ -2315,7 +2315,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
             return super.onPreferenceTreeClick(preference);
         }
 
-        return false;
+        return true;
     }
 
     private void startInactiveAppsFragment() {
