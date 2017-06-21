8e1e57be645478894f561ab2961e1dd1d8b5445c
acuicultor <ramoncuenca4@gmail.com>
2017-06-21 18:05:17 +0200

    Revert: Unlock keystore with fingerprint after reboot (2/2)
    adapted for nuclear, original commit from xyyx all creddits to him
    
    # Conflicts:
    #	res/values/nuclear_strings.xml (reverted from commit 846db3397008d03e155192a2b418059c5d4f1d7e)

--- src/com/android/settings/nuclear/tabs/LockScreen.java
+++ src/com/android/settings/nuclear/tabs/LockScreen.java
@@ -36,7 +36,6 @@ import com.android.internal.widget.LockPatternUtils;
 import com.android.settings.R;
 import com.android.settings.OwnerInfoSettings;
 import com.android.settings.SettingsPreferenceFragment;
-import android.hardware.fingerprint.FingerprintManager;
 import com.android.settings.Utils;
 
 import static android.provider.Settings.System.SCREEN_OFF_TIMEOUT; 
@@ -46,32 +45,17 @@ public class LockScreen extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "LockScreen";
     private static final String KEY_OWNER_INFO_SETTINGS = "owner_info_settings";
-    private static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
     private static final int MY_USER_ID = UserHandle.myUserId();
 
 
     private Preference mOwnerInfoPref;
     private LockPatternUtils mLockPatternUtils;
-    private SwitchPreference mFpKeystore;
-    private FingerprintManager mFingerprintManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final PreferenceScreen prefScreen = getPreferenceScreen();
 
         addPreferencesFromResource(R.xml.lockscreen);
-        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
- 
- 
-         mFpKeystore = (SwitchPreference) findPreference(FP_UNLOCK_KEYSTORE);
-         if (!mFingerprintManager.isHardwareDetected()){
-             prefScreen.removePreference(mFpKeystore);
-         } else {
-         mFpKeystore.setChecked((Settings.System.getInt(getContentResolver(),
-                 Settings.System.FP_UNLOCK_KEYSTORE, 0) == 1));
-         mFpKeystore.setOnPreferenceChangeListener(this);
-         }
 
         mLockPatternUtils = new LockPatternUtils(getContext());
 
@@ -109,17 +93,9 @@ public class LockScreen extends SettingsPreferenceFragment implements
                             : getString(R.string.owner_info_settings_summary));
     }
 
-   public boolean onPreferenceChange(Preference preference, Object objValue) {
-    
-    final String key = preference.getKey();
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
 
-        if (preference == mFpKeystore) {
-           boolean value = (Boolean) objValue;
-           Settings.System.putInt(getActivity().getContentResolver(),
-                   Settings.System.FP_UNLOCK_KEYSTORE, value ? 1 : 0);
-           return true;
-        }
-           
-        return false;
-   }
 }
